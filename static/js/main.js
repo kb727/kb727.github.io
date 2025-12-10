669cb4f176c4bb1d68e059093660b2d80fcc3a8d
minfangbiu <aspaccess@foxmail.com>
2025-12-10 17:27:55 +0800

    20251210172701

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,18 @@
 // Local-only version - Google Analytics removed
 
-$( document ).ready(function() {
+$(document).ready(function() {
+  // 初始化tooltip
   $("[rel='tooltip']").tooltip();
-});
-
-$("#menu-close").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
-
-$(function() {
+  
+  // 合并菜单切换功能
+  $("#menu-close, #menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+  
+  // 平滑滚动功能
   $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-      || location.hostname == this.hostname) {
-
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
@@ -28,31 +23,35 @@ $(function() {
       }
     }
   });
-
 });
 
+// 滚动触发动画（合并处理）
 $(window).scroll(function() {
-  $('.toSlideRight').each(function(){
-    var itemPos = $(this).offset().top;
+  $('.toSlideRight, .toSlideDown').each(function() {
+    var $this = $(this);
+    var itemPos = $this.offset().top;
     var topOfWindow = $(window).scrollTop();
-
-    if (itemPos < topOfWindow+600) {
-      $(this).addClass("slideRight");
-    }
-  });
-
-  $('.toSlideDown').each(function(){
-    var itemPos = $(this).offset().top;
-    var topOfWindow = $(window).scrollTop();
-
-    if (itemPos < topOfWindow+600) {
-      $(this).addClass("slideDown");
+    
+    if (itemPos < topOfWindow + 600) {
+      if ($this.hasClass('toSlideRight')) {
+        $this.addClass('slideRight');
+      }
+      if ($this.hasClass('toSlideDown')) {
+        $this.addClass('slideDown');
+      }
     }
   });
 });
 
-jQuery.scrollDepth({
-  elements: ['#portfolio', '#contact'],
-  percentage: false,
-  nonInteraction: false,
-});
+// scrollDepth配置 (添加错误处理)
+if (typeof jQuery.scrollDepth !== 'undefined') {
+  try {
+    jQuery.scrollDepth({
+      elements: ['#portfolio', '#contact'],
+      percentage: false,
+      nonInteraction: false,
+    });
+  } catch (e) {
+    console.warn('scrollDepth initialization failed:', e);
+  }
+}
